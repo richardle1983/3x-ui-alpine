985001cdef3561d05a2e4a234f96a8d3819f3194
lejibin <61679137+lejibin@users.noreply.github.com>
2024-12-13 01:19:46 +0800

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -117,11 +117,14 @@ install_x-ui() {
     cd x-ui
     mv app/* .
     rm -r app
+    rm DockerEntrypoint.sh
     chmod +x x-ui bin/xray-linux-amd64
     wget --no-check-certificate -O /etc/init.d/x-ui https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/x-ui.rc
     chmod +x /etc/init.d/x-ui
     wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/x-ui.sh
     chmod +x /usr/bin/x-ui
+    wget --no-check-certificate -O /usr/local/x-ui/DockerEntrypoint.sh https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/DockerEntrypoint.sh
+    chmod +x /usr/local/x-ui/DockerEntrypoint.sh
     config_after_install
 	rc-update add x-ui
 	rc-service x-ui start
