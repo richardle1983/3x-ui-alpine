1f27a5dc77777a277eae2240802f688feefd9a29
lejibin <61679137+lejibin@users.noreply.github.com>
2024-12-13 01:26:50 +0800

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -101,8 +101,21 @@ install_x-ui() {
             exit 1
         fi
     else
-	echo -e "${red}Download x-ui $1 failed, please check if the version exists ${plain}"
-        exit 1
+        tag_version=$1
+        tag_version_numeric=${tag_version#v}
+        min_version="2.4.8"
+        if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
+            echo -e "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
+            exit 1
+        fi
+
+        url="https://github.com/56idc/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-alpine.tar.gz"
+        echo -e "Beginning to install x-ui $1"
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-alpine.tar.gz ${url}
+        if [[ $? -ne 0 ]]; then
+            echo -e "${red}Download x-ui $1 failed, please check if the version exists ${plain}"
+            exit 1
+        fi
     fi
 
     if [[ -e /usr/local/x-ui/ ]]; then
