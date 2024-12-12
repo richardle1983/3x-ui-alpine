66bf34335ea66425426c3ad3ffa62d215c67263e
lejibin <61679137+lejibin@users.noreply.github.com>
2024-12-13 01:05:57 +0800

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -116,6 +116,7 @@ install_x-ui() {
     mv app/* .
     chmod +x x-ui bin/xray-linux-amd64
     wget --no-check-certificate -O /etc/init.d/x-ui https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/x-ui.rc
+    chmod +x /etc/init.d/x-ui
     wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/56idc/3x-ui-alpine/main/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
