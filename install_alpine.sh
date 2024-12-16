96c7085232be7832a8d5f71f49cab6bd6a75b111
lejibin <61679137+lejibin@users.noreply.github.com>
2024-12-16 12:21:33 +0800

    Update install_alpine.sh
    翻译中文

--- install_alpine.sh
+++ install_alpine.sh
@@ -8,7 +8,7 @@ plain='\033[0m'
 cur_dir=$(pwd)
 
 # check root
-[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
+[[ $EUID -ne 0 ]] && echo -e "${red}严重错误: ${plain} 请以 root 权限运行此脚本 \n " && exit 1
 
 install_base() {
 	apk add --no-cache --update ca-certificates tzdata fail2ban bash
@@ -41,44 +41,44 @@ config_after_install() {
             read -p "Would you like to customize the Panel Port settings? (If not, a random port will be applied) [y/n]: " config_confirm
             if [[ "${config_confirm}" == "y" || "${config_confirm}" == "Y" ]]; then
                 read -p "Please set up the panel port: " config_port
-                echo -e "${yellow}Your Panel Port is: ${config_port}${plain}"
+                echo -e "${yellow}您的面板端口是: ${config_port}${plain}"
             else
                 local config_port=$(shuf -i 1024-62000 -n 1)
-                echo -e "${yellow}Generated random port: ${config_port}${plain}"
+                echo -e "${yellow}生成的随机端口: ${config_port}${plain}"
             fi
 
             /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}" -port "${config_port}" -webBasePath "${config_webBasePath}"
-            echo -e "This is a fresh installation, generating random login info for security concerns:"
+            echo -e "这是全新安装，出于安全考虑，会生成随机登录信息:"
             echo -e "###############################################"
-            echo -e "${green}Username: ${config_username}${plain}"
-            echo -e "${green}Password: ${config_password}${plain}"
-            echo -e "${green}Port: ${config_port}${plain}"
-            echo -e "${green}WebBasePath: ${config_webBasePath}${plain}"
-            echo -e "${green}Access URL: http://${server_ip}:${config_port}/${config_webBasePath}${plain}"
+            echo -e "${green}用户名: ${config_username}${plain}"
+            echo -e "${green}密码: ${config_password}${plain}"
+            echo -e "${green}端口: ${config_port}${plain}"
+            echo -e "${green}面板路径: ${config_webBasePath}${plain}"
+            echo -e "${green}访问面板URL: http://${server_ip}:${config_port}/${config_webBasePath}${plain}"
             echo -e "###############################################"
-            echo -e "${yellow}If you forgot your login info, you can type 'x-ui settings' to check${plain}"
+            echo -e "${yellow}如果你忘记了登录信息，你可以使用命令“x-ui settings”${plain}"
         else
             local config_webBasePath=$(gen_random_string 15)
-            echo -e "${yellow}WebBasePath is missing or too short. Generating a new one...${plain}"
+            echo -e "${yellow}面板路径缺失或太短。正在生成一个新的...${plain}"
             /usr/local/x-ui/x-ui setting -webBasePath "${config_webBasePath}"
-            echo -e "${green}New WebBasePath: ${config_webBasePath}${plain}"
-            echo -e "${green}Access URL: http://${server_ip}:${existing_port}/${config_webBasePath}${plain}"
+            echo -e "${green}新的面板路径: ${config_webBasePath}${plain}"
+            echo -e "${green}访问面板URL: http://${server_ip}:${existing_port}/${config_webBasePath}${plain}"
         fi
     else
         if [[ "$existing_username" == "admin" && "$existing_password" == "admin" ]]; then
             local config_username=$(gen_random_string 10)
             local config_password=$(gen_random_string 10)
 
-            echo -e "${yellow}Default credentials detected. Security update required...${plain}"
+            echo -e "${yellow}检测到默认用户名和密码。需要安全更新...${plain}"
             /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}"
-            echo -e "Generated new random login credentials:"
+            echo -e "生成新的随机登录用户名和密码:"
             echo -e "###############################################"
-            echo -e "${green}Username: ${config_username}${plain}"
-            echo -e "${green}Password: ${config_password}${plain}"
+            echo -e "${green}用户名: ${config_username}${plain}"
+            echo -e "${green}密码: ${config_password}${plain}"
             echo -e "###############################################"
-            echo -e "${yellow}If you forgot your login info, you can type 'x-ui settings' to check${plain}"
+            echo -e "${yellow}如果你忘记了登录信息，你可以使用命令“x-ui settings”${plain}"
         else
-            echo -e "${green}Username, Password, and WebBasePath are properly set. Exiting...${plain}"
+            echo -e "${green}用户名、密码和面板路径已正确设置。退出...${plain}"
         fi
     fi
 
@@ -90,13 +90,13 @@ install_x-ui() {
     if [ $# == 0 ]; then
         tag_version=$(curl -Ls "https://api.github.com/repos/56idc/3x-ui-alpine/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$tag_version" ]]; then
-            echo -e "${red}Failed to fetch x-ui version, it may be due to GitHub API restrictions, please try it later${plain}"
+            echo -e "${red}获取x-ui版本失败，可能是GitHub API限制，请稍后重试...${plain}"
             exit 1
         fi
-        echo -e "Got x-ui latest version: ${tag_version}, beginning the installation..."
+        echo -e "获取x-ui最新版本: ${tag_version}, 开始安装..."
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-alpine.tar.gz https://github.com/56idc/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-alpine.tar.gz
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}Downloading x-ui failed, please be sure that your server can access GitHub ${plain}"
+            echo -e "${red}下载 x-ui 失败，请确保你的服务器可以访问 GitHub ${plain}"
             exit 1
         fi
     else
@@ -104,26 +104,30 @@ install_x-ui() {
         tag_version_numeric=${tag_version#v}
         min_version="2.4.8"
         if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}Please use a newer version (at least v2.4.8). Exiting installation.${plain}"
+            echo -e "${red}请使用较新的版本（至少 v2.4.8）。退出安装...${plain}"
             exit 1
         fi
 
         url="https://github.com/56idc/3x-ui-alpine/releases/download/${tag_version}/x-ui-linux-alpine.tar.gz"
-        echo -e "Beginning to install x-ui $1"
+        echo -e "开始安装x-ui $1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-alpine.tar.gz ${url}
         if [[ $? -ne 0 ]]; then
-            echo -e "${red}Download x-ui $1 failed, please check if the version exists ${plain}"
+            echo -e "${red}下载x-ui $1 失败，请检查版本是否存在 ${plain}"
             exit 1
         fi
     fi
 
     if [[ -e /usr/local/x-ui/ ]]; then
-      echo "Delete old version!!!"
+      echo -e "卸载旧版本..."
       rc-update del x-ui
       rc-service x-ui stop
       fail2ban-client -x stop
       rm /usr/local/x-ui/ -rf
       rm /etc/init.d/x-ui
+	  if [[ -e /app/bin/ ]]
+	    pgrep -f x-ui | xargs -r kill -9
+        rm /app -rf
+	  fi
     fi
 
     tar zxvf x-ui-linux-alpine.tar.gz
@@ -141,28 +145,28 @@ install_x-ui() {
     fail2ban-client -x start
     rc-update add x-ui default
     rc-service x-ui start
-    echo -e "${green}x-ui ${tag_version}${plain} installation finished, it is running now..."
+    echo -e "${green}x-ui ${tag_version}${plain} 安装完成, 运行中..."
     echo -e ""
     echo -e "x-ui control menu usages: "
     echo -e "----------------------------------------------"
     echo -e "SUBCOMMANDS:"
-    echo -e "x-ui              - Admin Management Script"
-    echo -e "x-ui start        - Start"
-    echo -e "x-ui stop         - Stop"
-    echo -e "x-ui restart      - Restart"
-    echo -e "x-ui status       - Current Status"
-    echo -e "x-ui settings     - Current Settings"
-    echo -e "x-ui enable       - Enable Autostart on OS Startup"
-    echo -e "x-ui disable      - Disable Autostart on OS Startup"
-    echo -e "x-ui log          - Check logs"
-    echo -e "x-ui banlog       - Check Fail2ban ban logs"
-    echo -e "x-ui update       - Update"
-    echo -e "x-ui legacy       - legacy version"
-    echo -e "x-ui install      - Install"
-    echo -e "x-ui uninstall    - Uninstall"
+    echo -e "x-ui              - 主菜单"
+    echo -e "x-ui start        - 运行服务"
+    echo -e "x-ui stop         - 停止服务"
+    echo -e "x-ui restart      - 重启服务"
+    echo -e "x-ui status       - 查看服务状态"
+    echo -e "x-ui settings     - 查看服务配置"
+    echo -e "x-ui enable       - 打开服务开机自动启动"
+    echo -e "x-ui disable      - 关闭服务开机自动启动"
+    echo -e "x-ui log          - 查看日志"
+    echo -e "x-ui banlog       - 查看Fail2ban日志"
+    echo -e "x-ui update       - 升级"
+    echo -e "x-ui legacy       - 安装旧版本"
+    echo -e "x-ui install      - 安装"
+    echo -e "x-ui uninstall    - 卸载"
     echo -e "----------------------------------------------"
 }
 
-echo -e "${green}Running...${plain}"
+echo -e "${green}运行中...${plain}"
 install_base
 install_x-ui $1
